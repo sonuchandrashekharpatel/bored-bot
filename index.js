2ca4aba9f3c982c30fc6af21b9ceee193433b103
sonuchandrashekharpatel <sonuchandrashekharpatel@gmail.com>
2026-04-17 17:07:20 +0530

    initial push of code

---
+++ index.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById("btn")
+
+btn.addEventListener("click", function(){
+
+  fetch('https://apis.scrimba.com/bored/api/activity')
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById("activity-name").textContent = data.activity
+      document.body.classList.add("idea-came")
+      document.getElementById("title").textContent = "🦾 HappyBot🦿"
+
+    })
+})
